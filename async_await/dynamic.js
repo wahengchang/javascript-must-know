290806860e7a9fc6838b78f2cde73c33a31ef0bb
peter <jy02927731@yahoo.com.hk>
2016-12-13 09:49:49 +0800

    simplify dynamic

--- async_await/dynamic.js
+++ async_await/dynamic.js
@@ -1,26 +1,17 @@
-var sleep = function(para) {
+var sleep = function(para1, para2) {
+    var _para1 = para1,
+        _para2 = para2 || para1;
     return new Promise(function(resolve, reject) {
         setTimeout(function() {
-            resolve(para * para)
+            resolve(_para1 * _para2)
         }, 1000)
     })
 }
-
-var sleep2para = function(para1, para2) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            resolve(para1 * para2)
-        }, 1000)
-    })
-}
-
 var proMap = new Map();
 proMap.set([1], sleep);
-proMap.set([2, 3], sleep2para);
+proMap.set([2, 3], sleep);
 proMap.set([3], sleep);
-
-
 for (var [para, fun] of proMap.entries()) {
     var result = await fun.apply(this, para);
-    console.log(result)
+    console.log(para, result)
 }
