31aff7ac06e3e8fd32b441c0e57351baa2267367
peter <jy02927731@yahoo.com.hk>
2016-12-13 09:52:22 +0800

    comment added

--- async_await/dynamic.js
+++ async_await/dynamic.js
@@ -7,11 +7,21 @@ var sleep = function(para1, para2) {
         }, 1000)
     })
 }
+
+//generate promsie stack
 var proMap = new Map();
+//proMap.set(<parameter array>, <function>)
 proMap.set([1], sleep);
 proMap.set([2, 3], sleep);
 proMap.set([3], sleep);
+
+//resolving promsie stack, sequentially
 for (var [para, fun] of proMap.entries()) {
+    // fun.apply is converting array to argument object
     var result = await fun.apply(this, para);
     console.log(para, result)
 }
+
+//[ 1 ] 1
+//[ 2, 3 ] 6
+//[ 3 ] 9
