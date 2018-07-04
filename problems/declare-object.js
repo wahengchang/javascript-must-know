3cf7f04928c8695dbed1f1abb985537fa8c90aa7
peterchang <wahengchang@gmail.com>
2018-07-04 10:18:47 +0800

    Create declare-object.js

---
+++ problems/declare-object.js
@@ -0,0 +1,61 @@
+/////////////// part 1 ////////////////
+const objConst = {body: {id: 1}}
+
+let objLet = {body: {id: 1}}
+
+// difference ?
+objConst = 2
+objLet = 2
+
+// difference ?
+objConst.body.id = 2
+objLet.body.id = 2
+
+
+////////////// part2.1  ///////////
+
+const objConst = {body: {id: 1}}
+
+const p1 = objConst.body
+let p2 = objConst.body
+
+// difference ?
+p1 = 2
+p2 = 3
+
+// difference ?
+p1.id = 2
+p2.id = 3
+
+
+////////////// part2.2  ///////////
+
+const objConst = {body: {id: 1}}
+
+const p1 = objConst.body
+const {body: p2} = objConst
+
+// difference ?
+p1 = 2
+p2 = 3
+
+// difference ?
+p1.id = 2
+p2.id = 3
+
+
+////////////// part3  ///////////
+
+const objConst = {body: {id: 1}}
+
+const p1 = objConst.body
+const p2 = {...objConst.body}
+
+// difference ?
+p1 = 2
+p2 = 2
+
+// difference ?
+p1.id = 2
+p2.id = 2
+
